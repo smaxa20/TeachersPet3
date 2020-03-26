967e4612034a729a67e78c7586ca22cf496b2df5
smaxa20 <scottkmaxa@gmail.com>
2020-03-26 01:36:14 -0700

    Layout class list.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,18 @@ function App() {
   return (
     <Styled.Page>
       <Styled.Header></Styled.Header>
-      <Styled.Body></Styled.Body>
+      <Styled.Body>
+        <Styled.ClassList>
+          <Styled.ClassListHeader>My Classes</Styled.ClassListHeader>
+          <Styled.ClassButton active>First Period</Styled.ClassButton>
+          <Styled.ClassButton>Second Period</Styled.ClassButton>
+          <Styled.ClassButton>Third Period</Styled.ClassButton>
+          <Styled.ClassButton>Fifth Period</Styled.ClassButton>
+          <Styled.ClassButton>Sixth Period</Styled.ClassButton>
+          <Styled.ClassButton>Homeroom</Styled.ClassButton>
+          <Styled.ClassButton>Study Hall</Styled.ClassButton>
+        </Styled.ClassList>
+      </Styled.Body>
       <Styled.Footer></Styled.Footer>
     </Styled.Page>
   );
--- src/styled.jsx
+++ src/styled.jsx
@@ -35,5 +35,46 @@ export const Footer = styled.div`
 export const Body = styled.div`
     min-height: 60vh;
     min-width: 100vw;
-    background-color: #DFE6D9;
+    background-color: #EEEFEB;
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const ClassList = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-content: flex-start;
+    align-items: flex-start;
+    max-width: 80vw;
+`;
+
+export const ClassListHeader = styled.div`
+    font-size: 24pt;
+    align-self: center;
+    margin: 16px 0px 4px;
+`;
+
+export const ClassButton = styled.button`
+    font-family: inherit;
+    width: 200px;
+    background-color: ${props => props.active ? '#EEEFEB' : '#436252'};
+    color: ${props => props.active ? '#436252' : '#EEEFEB'};
+    padding: 4px 8px 8px;
+    margin: 4px;
+    border: 1px solid #436252;
+    border-radius: 5px;
+    font-size: 12pt;
+    cursor: pointer;
+    &:focus {
+        outline: 0;
+    }
+    &:hover {
+        background-color: white;
+        color: #436252;
+    }
+    &:active {
+        background-color: #436252;
+        color: white;
+    }
 `;
